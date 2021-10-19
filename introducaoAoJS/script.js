582efa8a72ff6ed3096e793bfc5bdc5a69329b1e
Juniorkz13 <junior.13.kz@hotmail.com>
2021-10-18 22:16:20 -0300

    web

---
+++ introducaoAoJS/script.js
@@ -0,0 +1,23 @@
+alert('Olá tudo bem!')
+
+let a = 10
+let b = 'Teste'
+let c = 10.99
+let d = true
+let e = false
+let f = null
+//let g = undefined
+let g
+let h = {
+    nome: 'José',
+    saldo: 500.9
+}
+
+h.sexo = 'M'
+h.nome = 10
+let i = new Date()
+
+i.setMinutes(30)
+i.setSeconds(0)
+
+console.log(i.getHours(), i.getMinutes(), i.getSeconds())
---
+++ introducaoAoJS/script2.js
@@ -0,0 +1,3 @@
+const calcularArea = (raio) => Math.PI * raio ** 2
+
+alert(`A área do círculo é ${calcularArea(3)}`)
